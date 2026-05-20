b672441c35ab088817aa39def57bc5573a97314a
kuko010 <kukouwu@proton.me>
2026-05-20 17:26:18 +0200

    other

--- src/main/java/net/kuko/ish/Utils.java
+++ src/main/java/net/kuko/ish/Utils.java
@@ -2,13 +2,11 @@ package net.kuko.ish;
 
 
 public class Utils {
-/*
-    public static int packARGB(long packed) {
-        return new Color(
-                (packed >> 16 & 255) / 255f,
-                (packed >> 8 & 255) / 255f,
-                (packed & 255) / 255f,
-                (packed >> 24 & 255) / 255f
-        ).argb();
-    }*/
+    // Source - https://stackoverflow.com/q/70432097
+    // Posted by KepaArizzabalaga
+    // Retrieved 2026-03-29, License - CC BY-SA 4.0
+    public static int fromRGBA(final int red, final int green, final int blue, final int alpha) {
+        int argb = (0xFF & alpha) << 24 | (0xFF & red) << 16 | (0xFF & green) << 8 | (0xFF & blue);
+        return argb;
+    }
 }
